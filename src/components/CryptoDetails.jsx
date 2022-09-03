cd73575b1e5ea847094014138b6293c2eaa8ac9e
Hamid Hassani <hamidhassani119@gmail.com>
2022-09-03 20:40:51 +0430

    some changes happened in crypto details component

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import millify from "millify";
+import { useParams } from "react-router-dom";
+import { Col, Row, Typography, Select } from "antd";
+import { useGetCryptoDetailsQuery } from "../services/cryptoAPI";
+import {
+  MoneyCollectOutlined,
+  DollarCircleOutlined,
+  FundOutlined,
+  ExclamationCircleOutlined,
+  StopOutlined,
+  TrophyOutlined,
+  CheckOutlined,
+  NumberOutlined,
+  ThunderboltOutlined,
+} from "@ant-design/icons";
+
+const { Title, Text } = Typography;
+const { Option } = Typography;
 
 const CryptoDetails = () => {
-  return <div>CryptoDetails</div>;
+  const { coinId } = useParams();
+  const [timePeriod, setTimePeriod] = useState("7d");
+
+  const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
+
+  console.log(data);
+
+  return <div>CryptoDetails: {coinId}</div>;
 };
 
 export default CryptoDetails;
